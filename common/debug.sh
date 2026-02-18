eb0463550b6313c0a77a1051491fa1687f865215
Jacob Likins <jalikins05@gmail.com>
2026-02-17 19:06:31 -0500

    potential fix for the new ubuntu

--- common/debug.sh
+++ common/debug.sh
@@ -5,13 +5,31 @@
 # of issues finding the correct folder paths but I can make this more robust if 
 # needed -Jacob Likins
 
+# Check if openocd installed
+if ! command -v openocd &> /dev/null; then
+    echo "ERROR: 'openocd' is not installed."
+    echo "Please run: sudo apt install openocd"
+    exit 1
+fi
+
+# this should fix the issue with new ubuntu
+if command -v gdb-multiarch &> /dev/null; then
+    GDB_CMD="gdb-multiarch"
+elif command -v arm-none-eabi-gdb &> /dev/null; then
+    GDB_CMD="arm-none-eabi-gdb"
+else
+    echo "Please run: sudo apt install gdb-multiarch"
+    exit 1
+fi
+
+
 ELF_FILE="$1"
 
-# the Absolute Path of the ELF
+# the Path of the ELF
 ELF_ABS=$(realpath "$ELF_FILE")
 
 # Switch to the correct Project Root
-if [ -n "$BUILD_WORKING_DIRECTORY" ]; then # bazel gives us this
+if [ -n "$BUILD_WORKING_DIRECTORY" ]; then
     cd "$BUILD_WORKING_DIRECTORY"
 fi
 REPO_ROOT=$(pwd)
@@ -19,18 +37,13 @@ REPO_ROOT=$(pwd)
 # Calculates the Local Source Directory
 SOURCE_DIR=$(dirname "$1" | sed 's|bazel-bin/||' | sed 's|bazel-out/[^/]*/bin/||') 
 
-
-# Bazel creates a symlink named "bazel-<folder_name>" that points to the cache.
-# find it to locate where the HAL driver sources are hiding.
 WORKSPACE_NAME=$(basename "$REPO_ROOT")
 BAZEL_SYMLINK="bazel-$WORKSPACE_NAME"
 
 if [ -L "$BAZEL_SYMLINK" ]; then
     EXTERNAL_LIBS_PATH="$REPO_ROOT/$BAZEL_SYMLINK/external"
     echo "Found External Libs: $EXTERNAL_LIBS_PATH"
-    
     HAL_MAP_1="-ex \"set substitute-path external $EXTERNAL_LIBS_PATH\""
-    
     HAL_MAP_2="-ex \"set substitute-path /proc/self/cwd/external $EXTERNAL_LIBS_PATH\""
 else
     echo "Warning: Could not find bazel symlink. HAL debugging might fail."
@@ -41,6 +54,7 @@ fi
 echo "--------------------------------------------------"
 echo " Debugging: $ELF_FILE"
 echo " Repo Root: $REPO_ROOT"
+echo " Using GDB: $GDB_CMD"
 echo "--------------------------------------------------"
 
 # Kill Zombies
@@ -59,8 +73,8 @@ openocd -f interface/stlink.cfg -f target/stm32g4x.cfg -c "init; reset halt" > /
 OPENOCD_PID=$!
 sleep 2
 
-# Start GDB with External Mapping
-eval arm-none-eabi-gdb -tui "$ELF_ABS" \
+# Start GDB using the detected command
+eval $GDB_CMD -tui "$ELF_ABS" \
     -ex \"target remote localhost:3333\" \
     -ex \"monitor reset halt\" \
     -ex \"load\" \

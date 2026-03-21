05c73e0839788bc0c4785b1467fc07f0a2281e91
Jacob Likins <jalikins05@gmail.com>
2026-03-21 15:36:22 -0400

    fully tested and integrated system for CAN, will need file clean up tho but everything is proven to work

--- vehicle/mkviii/software/lv/throttle/throttle.c
+++ vehicle/mkviii/software/lv/throttle/throttle.c
@@ -4,30 +4,42 @@
 static void GpioInit(void);
 
 int main(void) {
-  HAL_Init();
-  SystemClockConfig();
-  GpioInit();
-  if (can_init_throttle() != 0) {
-        
+    HAL_Init();
+    SystemClockConfig();
+    GpioInit(); // Ensure this is still called 
+
+    // Turn LED OFF initially 
+    HAL_GPIO_WritePin(PA1_GPIO_Port, PA1_Pin, GPIO_PIN_RESET);
+
+    // Check Initialization
+    if (can_init_throttle() != 0) {
+        // If initialization fails, turn the LED ON SOLID.
+        HAL_GPIO_WritePin(PA1_GPIO_Port, PA1_Pin, GPIO_PIN_SET);
         while (1) {}
     }
 
     uint8_t dummy_data = 0;
+    uint8_t bspd_rx_count = 0;
 
-  while (1) {
+    while (1) {
+        can_poll_receive_all(); 
+
+        if (bspd.brake_gate == BRAKE_GATE_BRAKE_LIGHT_ON) { 
+            bspd_rx_count++; 
+        }
+
         throttle.test_counter = dummy_data; 
-
-
-
-        if (bspd.brake_gate == 1) {
-          if (can_send_throttle() == 0) {
+        throttle.test_counter_bspd = bspd_rx_count; 
+        
+        Check Transmission
+        if (can_send_throttle() == 0) {
             dummy_data++; 
-          }
-        } 
-
-
+      
+        }
+        HAL_GPIO_TogglePin(PA1_GPIO_Port, PA1_Pin);
         HAL_Delay(100);
     }
-  return 0;
+    
+    return 0;
 }
 

44ae5873d313d4ebcd27f2e5d31ed892d732807a
Jacob Likins <jalikins05@gmail.com>
2026-03-23 19:33:55 -0400

    Small README changes

--- vehicle/examples/can/dev_board/dev_board.h
+++ vehicle/examples/can/dev_board/dev_board.h
@@ -7,21 +7,6 @@
 #define PA1_Pin         GPIO_PIN_1
 
 
-void HAL_FDCAN_MspInit(FDCAN_HandleTypeDef *hfdcan) {
-    if (hfdcan->Instance == FDCAN1) {
-        __HAL_RCC_FDCAN_CLK_ENABLE();
-        __HAL_RCC_GPIOA_CLK_ENABLE();
-
-        // Configure PA11  and PA12
-        GPIO_InitTypeDef GPIO_InitStruct = {0};
-        GPIO_InitStruct.Pin = GPIO_PIN_11 | GPIO_PIN_12;
-        GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-        GPIO_InitStruct.Pull = GPIO_NOPULL;
-        GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
-        GPIO_InitStruct.Alternate = GPIO_AF9_FDCAN1;
-        HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-    }
-}
 
 //it uses the OEM dev board for STM32G441
 // Function Prototypes

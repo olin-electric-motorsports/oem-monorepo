295adc5127403622c5c0e23773e093654a7a83b7
MatthewT1107 <mtownsend1107@gmail.com>
2026-04-30 23:14:45 -0400

    Firmware update, CAN doesnt work at this stage, may or may not be my firmware

--- vehicle/mkviii/software/sensing/brake_temperature_sensor/brake_temp.c
+++ vehicle/mkviii/software/sensing/brake_temperature_sensor/brake_temp.c
@@ -1,15 +1,16 @@
 #include "brake_temp.h"
-
+#include "can_api.h"
 
 
 // Initialize Variables and Functions
 // Variable Declarations
 float bit_voltage = 0;
 float measured_voltage = 0;
-int temperature = 0;
-int delayval=500;
-
-
+uint16_t temperature = 0;
+int delayval = 500;
+uint16_t temperature_message = 0;
+//int can_init_brake_temp(void);
+//int can_send_brake_temperature(void);
 // Blinky
 
 
@@ -18,6 +19,8 @@ void init_peripherals(void) {
  SystemClockConfig();
  GpioInit();
  oem_adc_init(&brake_temp_sensor);
+ //can_init_brake_temp();
+ //can_send_brake_temperature(); // Send initial CAN message
  }
 
 // Trigger Debug LED
@@ -29,7 +32,7 @@ void update_debug(void) {
 
 void get_temperature(void) {
   //bit_voltage = oem_adc_read(&brake_temp_sensor);
-  bit_voltage = 3000;
+  bit_voltage = 500;
   HAL_Delay(10);
   measured_voltage = (bit_voltage / 4095.0) * 3.3;
   temperature = (measured_voltage - 0.5) * 200;
@@ -44,9 +47,18 @@ int main(void) {
   HAL_GPIO_TogglePin(GPIOA, HEARTBEAT_LED);
   get_temperature();
   delayval = 500;
+  
+  brake_temperature.temperature = temperature; //Can Testing
+  can_send_brake_temperature();
+
   if (measured_voltage >= 3.0) {
     update_debug();
   }
+  
+
+  //can_poll_receive_all();
+  //temperature_message = brake_temperature.temperature;
+  //temperature_message = brake_temperature.temperature;
  }
  return 0;
 }
